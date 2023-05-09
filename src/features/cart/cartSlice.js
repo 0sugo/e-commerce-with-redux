2aae2424ddb29b092da0cf64a0daaceafd8248e2
0sugo <josugo26@gmail.com>
2023-05-09 10:30:45 +0300

    Setup slice

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,14 @@
+import { CreateSlice, createSlice }  from "@reduxjs/toolkit";
+
+const initialState ={
+    cartItems:[],
+    amount:0,
+    total:0,
+    isLoading:true,
+
+}
+const cartSlice = createSlice({
+   name:'cart',
+   initialState 
+});
+export  default cartSlice;
--- src/store.js
+++ src/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cartSlice from "./features/cart/cartSlice";
 
 const store = configureStore({
     reducer:{},
-})
+});
+export default store;
